30b5119e6dc418d839296eeabbd0b6e277edd31f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-04-19 06:51:57 -0700

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        parser: {
+	  dataUrlCondition: {
+	    maxSize: 3 * 1024, // 3 KB
+	  },
+	},
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
