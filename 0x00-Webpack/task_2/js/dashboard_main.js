09d78d96d0f628eb1bcc181bc40f5f142e5c1b97
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-04-19 15:43:44 -0700

    Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,11 +2,10 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css/';
 
-/*
 $(document).ready(function() {
   const $body = $('body');
   const $count = $('#count');
-*/
+
   let count = 0;
 
   function updateCounter() {
@@ -14,7 +13,6 @@ $(document).ready(function() {
     return count;
   }
   
-$(function() {
   $body.append('<div id="logo"></div>')
   $body.append('<p>Holberton Dashboard</p>');
   $body.append('<p>Dashboard data for the students</p>');
