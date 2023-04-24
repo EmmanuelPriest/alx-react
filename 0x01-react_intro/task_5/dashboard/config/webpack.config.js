e5152300c857a3729f34bcc17ee78a171e0cf897
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-04-24 16:44:10 +0000

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,6 +8,45 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
+    hot: true,
     contentBase: './dist'
-  }
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+	  'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+	      bypassOnDebug: true,
+              disable: true,
+	    },
+	  },
+	]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Dashboard',
+      template: 'src/index.html'
+    })
+  ]
 };
