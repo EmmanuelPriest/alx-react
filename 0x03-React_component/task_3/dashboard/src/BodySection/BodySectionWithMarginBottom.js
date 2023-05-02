cff80159374639e87849a2dcc433abdbd7698f60
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-02 23:37:18 +0000

    Specialization - Create a new component BodySectionWithMarginBottom and its style file

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import BodySection from './BodySection';
-import './BodySectionWithMarginBottom.css';
+import React from "react";
+import PropTypes from "prop-types";
+import BodySection from "./BodySection";
+import "./BodySectionWithMarginBottom.css";
 
-const BodySectionWithMarginBottom = ({ title, children }) => {
+const BodySectionWithMarginBottom = ({ title, children, ...props }) => {
   return (
-    <div className="bodySectionWithMargin">
+    <div className="bodySectionWithMargin" {...props}>
       <BodySection title={title}>{children}</BodySection>
     </div>
   );
 };
 
 BodySectionWithMarginBottom.propTypes = {
-title: PropTypes.string.isRequired,
-children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default BodySectionWithMarginBottom;
