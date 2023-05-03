942bb21ead56e637334920b89a0f8a8748e9637f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-03 04:09:13 +0000

    Add a test with two checks for Notifications component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
 describe('Notifications', () => {
-  let wrapper;
-  const listNotifications = [    { id: 1, type: 'default', value: 'New course available' },    { id: 2, type: 'urgent', value: 'New resume available' },    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },  ];
-
-  beforeEach(() => {
-    wrapper = shallow(<Notifications listNotifications={listNotifications} />);
-  });
+  const wrapper = shallow(
+    <Notifications displayDrawer={true} listNotifications={[
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+    ]} />
+  );
 
   it('renders without crashing', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it('renders three list items', () => {
-    expect(wrapper.find('ul').children().length).toEqual(3);
+  it('does not rerender when updating with the same list', () => {
+    const shouldUpdate = wrapper.instance().shouldComponentUpdate({
+      displayDrawer: true,
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+      ]
+    });
+    expect(shouldUpdate).toBe(false);
   });
 
-  it('renders correct text', () => {
-    expect(wrapper.find('ul').childAt(0).text()).toEqual('New course available');
-    expect(wrapper.find('ul').childAt(1).text()).toEqual('New resume available');
-    expect(wrapper.find('ul').childAt(2).html()).toContain('<strong>Urgent requirement</strong> - complete by EOD');
-  });
-
-  it('has a close button', () => {
-    expect(wrapper.find('button[aria-label="close"]').exists()).toBe(true);
-  });
-
-  it('calls markAsRead function when clicked', () => {
-    const spy = jest.spyOn(console, 'log');
-    const instance = wrapper.instance();
-    const id = 1;
-    instance.markAsRead(id);
-    expect(spy).toHaveBeenCalledWith(`Notification ${id} has been marked as read`);
-    spy.mockRestore();
+  it('rerenders when updating with a longer list', () => {
+    const shouldUpdate = wrapper.instance().shouldComponentUpdate({
+      displayDrawer: true,
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+        { id: 4, type: 'default', value: 'New course available' },
+      ]
+    });
+    expect(shouldUpdate).toBe(true);
   });
 });
