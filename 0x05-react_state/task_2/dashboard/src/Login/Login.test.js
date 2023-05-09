f4546cee1586e8d6e52b39170012b89ebd4ef433
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-09 12:52:46 +0000

    Added Context in the App

--- 0x05-react_state/task_2/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_2/dashboard/src/Login/Login.test.js
@@ -29,4 +29,40 @@ describe('Login', () => {
 
     expect(submitButton.props().disabled).toBe(false);
   });
+
+  it('should call logIn function with email and password when form is submitted', () => {
+    const email = 'test@example.com';
+    const password = 'password';
+    const logInMock = jest.fn();
+    const context = { logIn: logInMock };
+    const wrapper = shallow(<Login />, { context });
+    const emailInput = wrapper.find('input[type="email"]');
+    const passwordInput = wrapper.find('input[type="password"]');
+    const submitButton = wrapper.find('input[type="submit"]');
+
+    emailInput.simulate('change', { target: { value: email } });
+    passwordInput.simulate('change', { target: { value: password } });
+
+    submitButton.simulate('submit', { preventDefault() {} });
+
+    expect(logInMock).toHaveBeenCalledWith(email, password);
+  });
+
+  it('should clear email and password fields after form is submitted', () => {
+    const email = 'test@example.com';
+    const password = 'password';
+    const context = { logIn: jest.fn() };
+    const wrapper = shallow(<Login />, { context });
+    const emailInput = wrapper.find('input[type="email"]');
+    const passwordInput = wrapper.find('input[type="password"]');
+    const submitButton = wrapper.find('input[type="submit"]');
+
+    emailInput.simulate('change', { target: { value: email } });
+    passwordInput.simulate('change', { target: { value: password } });
+
+    submitButton.simulate('submit', { preventDefault() {} });
+
+    expect(wrapper.state('email')).toBe('');
+    expect(wrapper.state('password')).toBe('');
+  });
 });
