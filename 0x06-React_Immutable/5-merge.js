dde35ff7efac12df8f49b33f302ec7a398f38e14
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-11 22:05:18 +0000

    Use list and map from the Immutable.js library to return a List containing the values of the two items that are each arrays and objects(aka concat and merge)

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,18 @@
+import { List, is } from 'immutable';
+
+function concatElements(page1, page2) {
+  const concatPage = List([page1, page2]).map((pageArr) => List(pageArr);
+  return concatPage;
+}
+
+function mergeElements(page1, page2) {
+  const mergePage = List([page1, page2]).map((pageObj) => {
+    if (is(pageObj, page2)) {
+      return List(Object.values(pageObj));
+    } else {
+      return List();
+    }
+  });
+}
+
+export default { concatElements, mergeElements };
