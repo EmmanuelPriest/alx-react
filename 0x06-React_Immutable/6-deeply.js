29669226c973ca1733b510473b8c812cb160cf60
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-11 23:52:04 +0000

    Use Map method from the Immutable.js library to return a List containing the values of the two items, if they are the same should combine both(aka Nested merge)

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,10 @@
 import { Map } from 'immutable';
 
 function mergeDeeplyElements(page1, page2) {
-  const mergeDeepPage = ((Map(page1)).mergeDeep(Map(page2)));
-  return mergeDeepPage;
+  const mergeDeepPage = Map(page1);
+  const mergedDeepPage = Map(page2);
+  const mergedDeepPages = mergeDeepPage.mergeDeep(mergedDeepPage);
+  return mergedDeepPages;
 }
 
 export { mergeDeeplyElements };
