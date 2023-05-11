20b95e09ab2a9035ef3f83893ff975e66bca10e6
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-11 13:17:06 +0000

    Function that accepts an obj and converts it into an immutable Map using fromJS of immutable.js library

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,17 @@
-const { fromJS } = require('immutable');
+const { fromJS, Map } = require('immutable');
 
-function getImmutableObject(object) {
+function getImmutableObject (object) {
   const immutableMap = fromJS(object);
+  return immutableMap;
 }
+
+/* Example */
+const object = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+const immutableObj = getImmutableObject(object);
+console.log(immutableObj);
