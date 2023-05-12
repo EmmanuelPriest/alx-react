2186dba11ac945f6e9970aac1397e875f9918b41
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-12 02:10:45 +0000

    Using Lazy Seq of the immutable.js library

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,7 +2,7 @@ import { Seq } from 'immutable';
 
 const printBestStudents(grades) {
   const lazySeq = Seq(grades);
-  const filteredGrades = Seq.filter((student) => { 
+  const filteredGrades = lazySeq.filter((student) => { 
     student.firstName.charAt(0).toUpperCase();
     const studentScore = student.score > 70;
     return studentScore;
@@ -16,7 +16,7 @@ const printBestStudents(grades) {
   const JSObject = filteredGrades.toJS();
 
   Object.keys(JSObject).map((map1) => {
-    JSobject[map1].firstName = firstLetter(JSObject[map1].firstName);
+    JSObject[map1].firstName = firstLetter(JSObject[map1].firstName);
     JSObject[map1].lastName = firstLetter(JSObject[map1].lastName);
     const obj = JSObject[map1];
     return obj;
