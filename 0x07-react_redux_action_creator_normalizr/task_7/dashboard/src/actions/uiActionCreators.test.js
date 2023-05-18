325bcd27e21d5a2345d79206170340eb7a063792
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-18 22:10:44 +0000

    Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -3,13 +3,53 @@ import {
   LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE
 } from './uiActionTypes';
 import {
   login,
   logout,
   displayNotificationDrawer,
   hideNotificationDrawer,
+  loginRequest
 } from './uiActionCreators';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fetch-mock';
+
+describe('loginRequest', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('dispatches LOGIN and LOGIN_SUCCESS actions on successful API response', () => {
+    fetchMock.getOnce('/login-success.json', { status: 200 });
+
+    const expectedActions = [
+      { type: 'LOGIN' },
+      { type: 'LOGIN_SUCCESS' },
+    ];
+    const store = mockStore({});
+
+    return store.dispatch(loginRequest('test@example.com', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('dispatches LOGIN and LOGIN_FAILURE actions on failed API response', () => {
+    fetchMock.getOnce('/login-success.json', { status: 404 });
+
+    const expectedActions = [
+      { type: 'LOGIN' },
+      { type: 'LOGIN_FAILURE' },
+    ];
+    const store = mockStore({});
+
+    return store.dispatch(loginRequest('test@example.com', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
 
 test('login action creator returns the correct action', () => {
   const email = 'test@example.com';
