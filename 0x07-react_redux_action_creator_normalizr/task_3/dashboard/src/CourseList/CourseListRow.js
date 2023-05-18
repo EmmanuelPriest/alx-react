429edaaf0529f69a9b46a3aba58a07c075eb8647
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-05-18 17:25:18 +0000

    Create actions for the course list

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  row: {
+    backgroundColor: '#f5f5f5ab',
+  },
+  headerRow: {
+    backgroundColor: '#deb5b545',
+  },
+  th: {
+    padding: '5px 10px',
+    textAlign: 'left',
+    fontWeight: 'bold',
+    borderBottom: '2px solid #ddd',
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4',
+  },
+});
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const rowStyle = css(styles.row, isHeader && styles.headerRow, isChecked && styles.rowChecked);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  if (isHeader) {
+    return (
+      <tr className={rowStyle}>
+        {textSecondCell === null ? (
+          <th colSpan="2" className={css(styles.th)}>
+            {textFirstCell}
+          </th>
+        ) : (
+          <>
+            <th className={css(styles.th)}>{textFirstCell}</th>
+            <th className={css(styles.th)}>{textSecondCell}</th>
+          </>
+        )}
+      </tr>
+    );
+  } else {
+    return (
+      <tr className={rowStyle}>
+        <td>
+          <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          {textFirstCell}
+        </td>
+        <td>{textSecondCell}</td>
+      </tr>
+    );
+  }
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+export default CourseListRow;
