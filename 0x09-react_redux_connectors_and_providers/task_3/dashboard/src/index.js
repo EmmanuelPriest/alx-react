a34e93d5eb1de8140f32069b3b3ab520079bcc70
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-06-24 14:42:16 +0000

    Understand how to use the Redux Chrome extension

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -10,7 +10,7 @@ import uiReducer from './reducers/uiReducer';
 
 const store = createStore(
   uiReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(),
   applyMiddleware(thunk)
 );
 
